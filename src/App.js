c70e238a74261886ed775e4bf1b3b9957e304f33
Nina Bosco <ninabosco1125@gmail.com>
2025-03-04 09:41:29 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./pages/Navbar";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import { TodoWrapper } from "./pages/TodoWrapper";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>
+    <div className="container">
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home/>} />
+          <Route path="todoWrapper" element={<TodoWrapper/>}/>
+          <Route path="contact" element={<Contact/>} />
+          <Route path="*" element={<NoPage/>} />
+        </Route>
+      </Routes>
     </div>
+    
+    </>
   );
 }
-
 export default App;
+
+/*<BrowserRouter>
+      
+    </BrowserRouter>*/
---
+++ src/readme.txt
@@ -0,0 +1,10 @@
+npx create-react-app project3
+cd project3
+    npm i web-vitals
+    npm i react-router-dom
+
+npm i uuid
+npm i --save @fortawesome/fontawesome-svg-core
+npm i --save @fortawesome/free-solid-svg-icons
+npm i --save @fortawesome/free-regular-svg-icons
+npm i --save @fortawesome/react-fontawesome@latest
